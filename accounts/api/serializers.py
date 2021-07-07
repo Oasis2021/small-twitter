bc7859d9ca843173c0e00782337da0aa4c29610e
erinl <erinqing.lin@gmail.com>
2021-07-06 22:15:07 -0700

    Account sign up

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -5,3 +5,42 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'email')
+
+class SignupSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=20, min_lenth=6)
+    password = serializers.CharField(max_length=20, min_lenth=6)
+    email = serializers.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+        def validate(self, data):
+            # Check username consists of only specific characters
+            if User.objects.filter(username=data['username'].lower()).exists():
+                raise exceptions.ValidationError({
+                    'message': 'This username has been used.'
+                })
+            if User.objects.filter(email=['email'].lower()).exists():
+                raise exceptions.ValidationError({
+                    'message': 'This email address has been occupied.'
+                })
+            return data
+
+        def create(self, validated_data):
+            username = validated_data['username'].lower()
+            email = validated_data['email'].lower()
+            password =validated_data['password']
+
+            user = User.objects.create_user(
+                username=username,
+                email=email,
+                password=password
+            )
+            return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+
